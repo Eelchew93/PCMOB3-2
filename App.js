e34a07b7879cbdf4f86c5a8a97d46d427abaf477
Eileen Chew <eelchew93@hotmail.sg>
2022-10-29 17:17:00 +0800

    First change

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from "react-native";
+import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
+import { Entypo } from "@expo/vector-icons";
+import * as SQLite from "expo-sqlite";
+import NotesStack from "./Screens/NotesStack";
+import AddScreen from "./Screens/AddScreen";
+
+const db = SQLite.openDatabase("notes.db");
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+   <NavigationContainer>
+     <Stack.Navigator mode="modal" headerMode="none">
+       <Stack.Screen
+         name="Notes Stack"
+         component={NotesStack}
+         options={{ headerShown: false }}
+       />
+       <Stack.Screen name="Add Note" component={AddScreen} />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+   flex: 1,
+   backgroundColor: "#ffc",
+   alignItems: "center",
+   justifyContent: "center",
+ },
 });
